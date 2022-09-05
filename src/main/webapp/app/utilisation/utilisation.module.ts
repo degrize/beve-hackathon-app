e1b45853f0c31ba8e269ddda24324c3266651251
Boakye2 <nanaboakyefred2@gmail.com>
2022-09-05 02:31:17 +0200

    implemenatation de la page vie privé , Regle & condition et les reseaux sociaux

--- src/main/webapp/app/utilisation/utilisation.module.ts
+++ src/main/webapp/app/utilisation/utilisation.module.ts
@@ -5,9 +5,10 @@ import { UTILISATION_ROUTE } from './utilisation.route';
 import { UtilisationComponent } from './utilisation.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @NgModule({
   declarations: [UtilisationComponent],
-  imports: [CommonModule, RouterModule.forChild([UTILISATION_ROUTE]), SharedModule],
+  imports: [CommonModule, RouterModule.forChild([UTILISATION_ROUTE]), SharedModule, CarouselModule],
 })
 export class UtilisationModule {}
